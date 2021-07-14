a55316f14c144b60c16191a65407c0640b9cdb26
Sajana Kavinda <49279952+sajjkavinda@users.noreply.github.com>
2021-07-14 22:43:12 +0530

    Add files via upload

---
+++ A2/MS1/contactHelpers.c
@@ -0,0 +1,79 @@
+//==============================================
+// Name:           Full name here
+// Student Number: #########
+// Email:          userID@myseneca.ca
+// Section:        XXX
+// Date:           
+//==============================================
+// Assignment:     2
+// Milestone:      1
+//==============================================
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+// This source file needs to "know about" the functions you prototyped
+//       in the contact helper header file.
+// HINT: Put the header file name in double quotes so the compiler knows
+//       to look for it in the same directory/folder as this source file
+//       #include your contactHelpers header file on the next line:
+
+
+
+//--------------------------------
+// Function Definitions
+//--------------------------------
+
+// +-------------------------------------------------+
+// | ====== Assignment 2 | Milestone 1 =======       |
+// +-------------------------------------------------+
+// | Put empty function definitions below...         |
+// |                                                 |
+// | - The clearKeyboard function is done for you    |
+// +-------------------------------------------------+
+
+// clearKeyboard:  Empty function definition 
+void clearKeyboard(void)
+{
+
+}
+
+// pause: Empty function definition goes here:
+void pause(void){
+
+}
+
+
+// getInt: Empty function definition goes here:
+int getInt(void){
+
+return 0;
+}
+
+
+// getIntInRange: Empty function definition goes here:
+
+int getIntInRange(int min, int max){
+
+return 0;
+}
+
+// yes: Empty function definition goes here:
+int yes(void){
+
+return 0;
+}
+
+
+// menu: Empty function definition goes here:
+int menu(void){
+
+return 0;
+}
+
+
+// contactManagerSystem: Empty function definition goes here:
+void contactManagerSystem(void){
+
+}
